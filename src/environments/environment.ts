24583c02e154f000b39f77d6aab7776179f8cf06
Dawid Adachowski <dadachowski@gmail.com>
2024-12-08 22:14:53 +0100

    set dev env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  API_URL:'http://localhost:8080',
+  API_URL:'http://192.168.0.165:8080',
   companyName: 'Multi Solar Sp. z o. o.',
   companyAddress: '61-324 Poznań, ul. Bocheńska 11',
   emailContact: 'biuro@multi-solar.pl'
