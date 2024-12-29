d9b37d40e0f2d6e3bbb898807c2271489e3369ee
Dawid Adachowski <dadachowski@gmail.com>
2024-12-29 17:18:15 +0100

    deploy 0.1.0v

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  API_URL:'http://192.168.0.165:8080',
+  API_URL:'https://api.wycenpv.pl',
   companyName: 'Multi Solar Sp. z o. o.',
   companyAddress: '61-324 Poznań, ul. Bocheńska 11',
   emailContact: 'biuro@multi-solar.pl'
